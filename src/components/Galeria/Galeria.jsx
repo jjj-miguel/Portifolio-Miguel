93c95caf478ae846b97629ba8f996e7526e20258
jjj-miguel <joaomiguelcarlos463@gmail.com>
2024-06-21 14:15:01 -0300

    configurando as rotas da galeria com link to

--- src/components/Galeria/Galeria.jsx
+++ src/components/Galeria/Galeria.jsx
@@ -1,6 +1,7 @@
 // src/components/App.jsx
 // eslint-disable-next-line no-unused-vars
 import React, { useState, useEffect } from 'react';
+import {Link } from 'react-router-dom';
 import styles from './Galeria.module.css';
 import trofeu from '../../assets/trofeu.png';
 import perfil from '../../assets/perfil.png';
@@ -10,13 +11,24 @@ function App() {
   return (
     <div className={styles.ContainerGaleria}>
       <div className={styles.Galeria}>
-        <div className={styles.ItemGaleria}>
-          <img src={trofeu} alt="Placeholder"  />
-        </div>
-        <div className={styles.ItemGaleria}>
-          <img src={perfil} alt="Placeholder" />
-        </div>
-        {/* Adicione mais itens da galeria aqui */}
+
+      <Link to="/page2">
+      <div  className={styles.ItemGaleria}>
+          <img src={perfil} 
+          alt="Placeholder"
+          className={styles.ItemGaleria}  />
+          </div>
+      </Link>
+      <Link to="/page1">
+          <div  className={styles.ItemGaleria}>
+          <img src={trofeu} 
+          alt="Placeholder" />
+          </div>
+          
+          
+
+      </Link>
+
       </div>
     </div>
   );
