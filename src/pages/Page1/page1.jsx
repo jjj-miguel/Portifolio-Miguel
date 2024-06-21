7fbb8ae1f0485e2b8037136f9107e98edccf00e1
jjj-miguel <joaomiguelcarlos463@gmail.com>
2024-06-21 14:17:33 -0300

    add estrutura e estilo da page 1, junto com as animações

--- src/pages/Page1/page1.jsx
+++ src/pages/Page1/page1.jsx
@@ -1,12 +1,39 @@
-// eslint-disable-next-line no-unused-vars
-import React from 'react'
+import style from '../Page1/page.module.css';
+import Navbar from '../../components/Navbar2/NavBar2';
+import React, { useState } from 'react';
 
-function page1() {
+
+function App() {
+  const [positions, setPositions] = useState(['center', 'right', 'left']);
+
+  const handleNext = () => {
+    setPositions((prevPositions) => {
+      return [prevPositions[2], prevPositions[0], prevPositions[1]];
+    });
+  };
+
   return (
-    <div>
-      <h1>jdsodsojd</h1>
+    <div className={style.App}>
+      <Navbar />
+      <div className={style.text_home}>
+        <h1 className={style.text_h1}>
+          Bem Vindo a Minha Sala de Troféus
+        </h1>
+      </div>
+      <div className={style.Container2}>
+        <div className={`${style.box} ${style.box1} ${style[positions[0]]}`}>
+          1º Lugar
+        </div>
+        <div className={`${style.box} ${style.box2} ${style[positions[1]]}`}>
+          2º Lugar
+        </div>
+        <div className={`${style.box} ${style.box3} ${style[positions[2]]}`}>
+          3º Lugar
+        </div>
+      </div>
+      <button className={style.button} onClick={handleNext}>Next</button>
     </div>
-  )
+  );
 }
 
-export default page1
+export default App;
